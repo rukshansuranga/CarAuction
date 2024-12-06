02670a2f932c9e2bb0554b020a6fd9755fb1d785
Rukshan Perera <rukshansuranga@gmail.com>
2024-12-06 06:46:36 +0530

    Init

---
+++ src/AuctionService/DTOs/AuctionDto.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace AuctionService.DTOs;
+
+public class AuctionDto
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; } = string.Empty;
+    public string Winner { get; set; } = string.Empty;
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; set; }
+    public string Status { get; set; }
+    public string Make { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public string? Color { get; set; }
+    public int Mileage { get; set; }
+    public string? ImageUrl { get; set; }
+}
